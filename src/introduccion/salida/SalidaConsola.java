fde0d76fe29aa9728a2c4f9a73cbf3f710412e51
nazarenomm <nazarenomm98@gmail.com>
2023-09-21 15:43:39 -0300

    21/9

--- src/introduccion/salida/SalidaConsola.java
+++ src/introduccion/salida/SalidaConsola.java
@@ -1,7 +1,11 @@
 package introduccion.salida;
-// si no lo hago estatico deberia crear un obj de clase SalidaConsola
+
 public class SalidaConsola {
     public static void mostrar(String frase){
         System.out.println("Escribiste: " + frase);
     }
+public static void main(String[] args) {
+    mostrar("hola mundo");
+}
+
 }
