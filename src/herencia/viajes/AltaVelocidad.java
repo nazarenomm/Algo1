b8c7afd4f9a6c7ad25cbc03653926a72f74f9eb7
nazarenomm <nazarenomm98@gmail.com>
2023-09-22 15:29:05 -0300

    Viajes

---
+++ src/herencia/viajes/AltaVelocidad.java
@@ -0,0 +1,12 @@
+package herencia.viajes;
+
+public class AltaVelocidad extends Viaje{
+    public AltaVelocidad(Trayecto trayecto, int cantVagones, int cantMaxPasajeros) {
+        super(trayecto, cantVagones, cantMaxPasajeros);
+    }
+    @Override
+    public double tiempoDemora() {
+        return trayecto.getDistancia() / 10.0;
+    }
+}
+    
