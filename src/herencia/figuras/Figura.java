102bac03c8b716d95a28f80d6b0cd33c82363d5e
nazarenomm <nazarenomm98@gmail.com>
2023-09-23 14:47:49 -0300

    figuras

---
+++ src/herencia/figuras/Figura.java
@@ -0,0 +1,16 @@
+package herencia.figuras;
+
+abstract public class Figura {
+
+    protected String nombre;
+
+    abstract protected double calcularArea();
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
